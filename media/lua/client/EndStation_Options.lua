b0915c45e830e575152db8e946760bdf76fe5217
Glytch3r <glytch3r.arts@gmail.com>
2025-02-21 14:29:21 +0800

    5th refactor

--- media/lua/client/EndStation_Options.lua
+++ media/lua/client/EndStation_Options.lua
@@ -16,157 +16,126 @@
 |                       		                                    														 	  |
 |                       	Support:    https://ko-fi.com/glytch3r														    	  |
 |_______________________________________________________________________________________________________________________________-]]
-
 EndStation = EndStation or {}
 
 local mode = SandboxVars.EndStation.TriggerModes or 4
 EndStation.currentTriggerMode = mode
+EndStation.shouldSync = false
 
-if mode == "1" then
-    Events.EveryOneMinute.Add(EndStation.deviceHandler)
-elseif EndStation.is10MinTrigger() then
-    Events.EveryTenMinutes.Add(EndStation.deviceHandler)
-elseif mode == "4" then
-    Events.EveryHours.Add(EndStation.deviceHandler)
-elseif mode == "5" then
-    Events.EveryDays.Add(EndStation.deviceHandler)
+EndStation.TriggerEvents = {
+    ['1'] = { event = Events.EveryOneMinute, condition = function() return SandboxVars.EndStation.TriggerModes == 1 end },
+    ['2'] = { event = Events.EveryTenMinutes, condition = function() return SandboxVars.EndStation.TriggerModes == 2 end },
+    ['3'] = { event = Events.EveryTenMinutes, condition = function() return SandboxVars.EndStation.TriggerModes == 3 and EndStation.IsThirtyMinutesPast() end },
+    ['4'] = { event = Events.EveryHours, condition = function() return SandboxVars.EndStation.TriggerModes == 4 end },
+    ['5'] = { event = Events.EveryDays, condition = function() return SandboxVars.EndStation.TriggerModes == 5 end }
+}
+
+EndStation.currentEvent = nil
+
+function EndStation.preDeviceHandler()
+    if EndStation.TriggerEvents[tostring(SandboxVars.EndStation.TriggerModes)] and EndStation.TriggerEvents[tostring(SandboxVars.EndStation.TriggerModes)].condition() then
+        EndStation.deviceHandler()
+    end
 end
 
+function EndStation.retrigger(mode)
+    if EndStation.currentEvent then
+        EndStation.currentEvent.event.Remove(EndStation.preDeviceHandler)
+    end
 
-function EndStation.getSound()
-    return SandboxVars.EndStation.Sound or "EndStation_Broadcast1"
+    local trigger = EndStation.TriggerEvents[tostring(mode)]
+    if trigger then
+        trigger.event.Add(EndStation.preDeviceHandler)
+        EndStation.currentEvent = trigger
+    else
+        EndStation.currentEvent = nil
+    end
 end
 
------------------------            ---------------------------
 function EndStation.saveSandbox()
     local sOpt = getSandboxOptions()
-    local opt1 = SandboxVars.EndStation.RadioDetectDistance
-    local opt2 = SandboxVars.EndStation.Sound
-    local opt3 = SandboxVars.EndStation.ShouldShowText
-    local opt4 = SandboxVars.EndStation.TriggerModes
+    local options = {
+        RadioDetectDistance = SandboxVars.EndStation.RadioDetectDistance,
+        Sound = SandboxVars.EndStation.Sound,
+        ShouldShowText = SandboxVars.EndStation.ShouldShowText,
+        TriggerModes = SandboxVars.EndStation.TriggerModes
+    }
 
-    sOpt:getOptionByName("EndStation.RadioDetectDistance"):setValue(tostring(opt1))
-    sOpt:getOptionByName("EndStation.Sound"):setValue(tostring(opt2))
-    sOpt:getOptionByName("EndStation.ShouldShowText"):setValue(tostring(opt3))
-    sOpt:getOptionByName("EndStation.TriggerModes"):setValue(tostring(opt4))
+    for key, value in pairs(options) do
+        sOpt:getOptionByName("EndStation." .. key):setValue(tostring(value))
+    end
+
     sOpt:toLua()
     sOpt:sendToServer()
     print("EndStation SandboxVars Synced")
+
+    EndStation.shouldSync = false
+    EndStation.retrigger(SandboxVars.EndStation.TriggerModes)
 end
------------------------            ---------------------------
-
-function EndStation.triggerHandler()
-    if EndStation.isSandboxChanged() then
-        EndStation.saveSandbox()
-        EndStation.retrigger(tostring(SandboxVars.EndStation.TriggerModes))
-    end
-end
-Events.EveryOneMinute.Add(EndStation.triggerHandler)
-
-
 
 function EndStation.isSandboxChanged()
-    local newMode = tostring(SandboxVars.EndStation.TriggerModes)
-    local curMode = tostring(EndStation.currentTriggerMode)
+    local newMode = SandboxVars.EndStation.TriggerModes
+    local curMode = EndStation.currentTriggerMode
+
+    if EndStation.shouldSync then
+        EndStation.saveSandbox()
+        EndStation.shouldSync = false
+    end
+
     if newMode ~= curMode then
+        EndStation.shouldSync = true
         EndStation.currentTriggerMode = newMode
-        getPlayer():addLineChatElement('EndStation: sandbox updated')
+        getPlayer():addLineChatElement("EndStation: sandbox updated")
         return true
     end
-end
-function EndStation.retrigger(mode)
-      EndStation.clearTriggers()
-    if mode == "1" then
-        Events.EveryOneMinute.Add(EndStation.deviceHandler)
-    elseif EndStation.is10MinTrigger() then
-        Events.EveryTenMinutes.Add(EndStation.deviceHandler)
-    elseif mode == "4" then
-        Events.EveryHours.Add(EndStation.deviceHandler)
-    elseif mode == "5" then
-        Events.EveryDays.Add(EndStation.deviceHandler)
-    end
-end
-
-function EndStation.clearTriggers()
-    if getCore():getDebug() or EndStation.dbg then
-        print("EndStation: Triggers Cleared")
-	end
-    Events.EveryOneMinute.Remove(EndStation.deviceHandler)
-    Events.EveryTenMinutes.Remove(EndStation.deviceHandler)
-    --Events.EveryTenMinutes.Remove(EndStation.deviceHandler)
-    Events.EveryHours.Remove(EndStation.deviceHandler)
-    Events.EveryDays.Remove(EndStation.deviceHandler)
 end
 
 function EndStation.is10MinTrigger()
     local mode = SandboxVars.EndStation.TriggerModes or 4
-    local TriggerModesTable = {
-        ['1'] = false,
-
-        ['2'] = true,
-        ['3'] = true,
-
-        ['4'] = false,
-        ['5'] = false,
-    }
-
-    return TriggerModesTable[mode]
+    local TriggerModesTable = { ['1'] = false, ['2'] = true, ['3'] = true, ['4'] = false, ['5'] = false }
+    return TriggerModesTable[tostring(mode)]
 end
-
 
 function EndStation.IsThirtyMinutesPast()
     return getGameTime():getMinutes() == 30
 end
 
-
 function EndStation.deviceHandler()
-    local bool = true
-    local mode = tostring(SandboxVars.EndStation.TriggerModes)
-    if EndStation.is10MinTrigger() and mode  == "3" then
-        if not EndStation.IsThirtyMinutesPast() then
-            bool = false
+    if EndStation.isSandboxChanged() then
+        EndStation.shouldSync = true
+        EndStation.saveSandbox()
+    end
+
+    local pl = getPlayer()
+    local part = EndStation.getTurnedOnVehicleRadio()
+    local item = EndStation.getPlayerRadio()
+    local obj = EndStation.getEndStationRadioInRange() or item or part
+
+    if obj then
+        if SandboxVars.EndStation.ShouldShowText or EndStation.dbg then
+            local msg = "EndStation: " .. tostring(obj:getDeviceData():getDeviceName()) .. " " .. tostring(obj:getDeviceData():getChannel())
+            print(msg)
+            pl:addLineChatElement(msg)
+        end
+
+        local sfx = EndStation.channelData(obj)
+        if sfx then
+            getSoundManager():playUISound(sfx)
         end
     end
-    if bool then
-        local pl = getPlayer()
-        local part = EndStation.getTurnedOnVehicleRadio()
-        local item = EndStation.getPlayerRadio()
-        local obj = EndStation.getEndStationRadioInRange() or item or part
-
-        if obj then
-            if SandboxVars.EndStation.ShouldShowText or EndStation.dbg then
-                local msg = "EndStation: " .. tostring(obj:getDeviceData():getDeviceName()) .. " " .. tostring(obj:getDeviceData():getChannel())
-                print(msg)
-                pl:addLineChatElement(msg)
-            end
-
-            local sfx = EndStation.channelData(obj)
-            if sfx then
-                getSoundManager():playUISound(sfx)
-            end
-        end
-    end
-end
-
------------------------            ---------------------------
-
-function ISServerSandboxOptionsUI:onButtonApply()
-    --EndStation.retrigger(tostring(SandboxVars.EndStation.TriggerModes))
-   	if isAdmin() or  EndStation.dbg then
-        print('EndStation: sandbox updated')
-        getPlayer():addLineChatElement(tostring('EndStation: sandbox updated'))
-	end
-    EndStation.saveSandbox()
---[[     self:settingsFromUI(self.options)
-	self.options:sendToServer()
-	self:destroy() ]]
-	return hook(self)
 end
 
 local hook = ISServerSandboxOptionsUI.onButtonApply
 function ISServerSandboxOptionsUI:onButtonApply()
-
+    hook(self)
+    EndStation.saveSandbox()
+    if isAdmin() or EndStation.dbg then
+        print("EndStation: sandbox updated")
+        getPlayer():addLineChatElement("EndStation: sandbox updated")
+    end
 end
+
+
 
 --[[_____________________________________________________________________________________________________________________________
    ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
